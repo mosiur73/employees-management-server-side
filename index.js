323bf916e7fc5f845d04652f8007956609fee051
mosiur49 <bpi2021cmt44@gmail.com>
2025-01-15 18:53:34 +0600

    jwt token create

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -30,6 +31,30 @@ async function run() {
     const userCollection = client.db("employee").collection("users");
     const employCollection = client.db("employee").collection("employ");
 
+    //jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+    //middleware
+    const verifyToken=(req,res,next)=>{
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+      
+    }
+
+
     //user related api 
     app.post('/users', async (req, res) => {
         const user = req.body;
@@ -42,6 +67,12 @@ async function run() {
        const result =await userCollection.insertOne(user)
         res.send(result);
       });
+
+      app.get('/users',verifyToken, async(req,res) =>{
+        console.log(req.headers);
+        const result=await userCollection.find().toArray()
+        res.send(result)
+      })
       
 
       //employee data api
